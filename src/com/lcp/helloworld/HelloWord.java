2a21e70aa25686225654cde25bc918e3a89b608b
cli <cli@linkernetworks.com>
2017-03-21 15:06:55 +0800

    11

--- src/com/lcp/helloworld/HelloWord.java
+++ src/com/lcp/helloworld/HelloWord.java
@@ -16,8 +16,9 @@ public class HelloWord {
 	}
 	
 	public static class subway{
-		public static void geton(){
-			System.out.println("hi,i am get on the subway,time is 08:15am");
+		public static  void geton(){
+			String x="hi,i am get on the subway,time is 08:15am";
+			System.out.println(x);
 		}
 		public static void getoff(){
 			System.out.println("hi,i am get off the subway,time is 08:55am");
