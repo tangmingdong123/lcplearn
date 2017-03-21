aeae1ec735846d32035c418dff29d64fcf83d3de
cli <cli@linkernetworks.com>
2017-03-21 14:17:23 +0800

    11

--- src/com/lcp/helloworld/HelloWord.java
+++ src/com/lcp/helloworld/HelloWord.java
@@ -5,6 +5,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello Licp! I'm xiao tang");
+		System.out.println("Hello xiaotang! I'm xiao pingzi");
 	}
 
 }
