2c19c5f751259929e74a574654ba561f2019e663
cli <cli@linkernetworks.com>
2017-03-21 14:42:50 +0800

    11

--- src/com/lcp/helloworld/HelloWord.java
+++ src/com/lcp/helloworld/HelloWord.java
@@ -4,12 +4,23 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 	    call.tiaoxi();
+	    subway.geton();
+	    subway.getoff();
 	}
 	
 	public static class call{
 		public static void tiaoxi(){
 			System.out.println("hi,i am xiaotang");
 			System.out.println("hi xiaotang,i am xiao pingzi");
+		}
+	}
+	
+	public static class subway{
+		public static void geton(){
+			System.out.println("hi,i am get on the subway,time is 08:15am");
+		}
+		public static void getoff(){
+			System.out.println("hi,i am get off the subway,time is 08:55am");
 		}
 	}
 
