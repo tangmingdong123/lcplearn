221599970b99006a449d9d81502d3c1276811c6d
cli <cli@linkernetworks.com>
2017-03-21 17:40:59 +0800

    format code

--- src/com/lcp/loop/learn/LoopLearn.java
+++ src/com/lcp/loop/learn/LoopLearn.java
@@ -1,27 +1,28 @@
 package com.lcp.loop.learn;
 
 public class LoopLearn {
-	
-	public static void main(String[] args){
-		
-		int a=0;
-		int b=0;
-		
-		for(int i=1;i<=1000;i++){
-			if (i%7==0)	 {   
-		        System.out.println("能整除7的为："+i);
-		        a++;
+
+	public static void main(String[] args) {
+
+		int a = 0;
+		int b = 0;
+
+		for (int i = 1; i <= 1000; i++) {
+			if (i % 7 == 0) {
+				System.out.println("能整除7的为：" + i);
+				a++;
 			}
-	}
-		System.out.println("包含7的共有："+a+"个数字");
-		
-		for(int i=1;i<=1000;i++){
-			if (i%1==7||i%10==7||i%11==7||i%100==7||i%101==7||i%110==7||i%111==7){	    
-		        System.out.println("包含7的："+i);
-			    b++;
+		}
+		System.out.println("包含7的共有：" + a + "个数字");
+
+		for (int i = 1; i <= 1000; i++) {
+			if (i % 1 == 7 || i % 10 == 7 || i % 11 == 7 || i % 100 == 7 || i % 101 == 7 || i % 110 == 7
+					|| i % 111 == 7) {
+				System.out.println("包含7的：" + i);
+				b++;
 			}
+		}
+		System.out.println("包含7的共有：" + b + "个数字");
+
 	}
-		System.out.println("包含7的共有："+b+"个数字");
-	
-	}	
 }
