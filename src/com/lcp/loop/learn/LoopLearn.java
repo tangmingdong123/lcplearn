f9ee4c439d7266c3b32939e85452fe9101e6e399
cli <cli@linkernetworks.com>
2017-03-21 17:38:07 +0800

    数字统计

--- src/com/lcp/loop/learn/LoopLearn.java
+++ src/com/lcp/loop/learn/LoopLearn.java
@@ -3,13 +3,25 @@ package com.lcp.loop.learn;
 public class LoopLearn {
 	
 	public static void main(String[] args){
+		
+		int a=0;
+		int b=0;
+		
 		for(int i=1;i<=1000;i++){
-			if (i%7==0)	    
+			if (i%7==0)	 {   
 		        System.out.println("能整除7的为："+i);
+		        a++;
+			}
 	}
+		System.out.println("包含7的共有："+a+"个数字");
+		
 		for(int i=1;i<=1000;i++){
-			if (i%1==7||i%10==7||i%100==7)	    
+			if (i%1==7||i%10==7||i%100==7){	    
 		        System.out.println("包含7的："+i);
+			    b++;
+			}
 	}
+		System.out.println("包含7的共有："+b+"个数字");
+	
 	}	
 }
