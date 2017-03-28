aaf7dd2c031a2bade53056943bc68d96b96c0992
cli <cli@linkernetworks.com>
2017-03-28 11:18:10 +0800

    11

--- src/com/work/ListImages.java
+++ src/com/work/ListImages.java
@@ -5,7 +5,11 @@ import com.work.json.JSONTool;
 
 public class ListImages {
 	public static void main(String[] args) throws Exception {
+		//brief=true,return only names of images
 		String json = HttpUtils.get("http://35.185.152.118:3001/images?brief=true");
 		System.out.println(JSONTool.formatJson(json));
+		//if false, return all fields of images doc
+		String json1 = HttpUtils.get("http://35.185.152.118:3001/images");
+		System.out.println(JSONTool.formatJson(json1));
 	}
 }
