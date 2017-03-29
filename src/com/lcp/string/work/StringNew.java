a7c46ac3f382e160b9fca47d127ee4e878287f0b
cli <cli@linkernetworks.com>
2017-03-29 11:00:40 +0800

    length

--- src/com/lcp/string/work/StringNew.java
+++ src/com/lcp/string/work/StringNew.java
@@ -2,8 +2,10 @@ package com.lcp.string.work;
 
 public class StringNew {
 	public static void main(String[] args){
-		char[] helloArray = {'l','i','n','k','e','r'};
+		char[] helloArray = {'l','i','n','k','e','r',' '};
 		String helloString = new String(helloArray);
 		System.out.println(helloString);
+		int len = helloString.length();
+		System.out.println("length:"+len);
 	}
 }
