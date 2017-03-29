a821bb2174294a729ba1c88c6cef834d2c4cf8c0
cli <cli@linkernetworks.com>
2017-03-29 20:19:10 +0800

    finish commit

--- src/com/lcp/string/work/StringNew.java
+++ src/com/lcp/string/work/StringNew.java
@@ -1,18 +1,20 @@
 package com.lcp.string.work;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 public class StringNew {
 	public static void main(String[] args) {
 		String source = "  肃宁县地处海河冲积平原，主要种植小麦、玉米等农作物，是国家级商品粮基地，是河北省核心区产粮大县之一；工业以毛皮加工、针纺服装、食品加工、图书印刷发行为主导产业，2013年完成地区生产总值130.0978亿元京九铁路、朔黄铁路在肃宁境内交叉而过，肃宁北站是西煤东运的重要铁路枢纽 12345678ABC901234567890abc123456789  ";
-		//countfor(source);
-		//countwhile(source);
-		//GDP(source);
-		//System.out.println("该句话共" + source.length() + "个字符");
-		//System.out.println("该句话去除首尾空格共" + source.trim().length() + "个字符");
-		//newline(source);
-		//changeencode(source);
+		countfor(source);
+		countwhile(source);
+		GDP(source);
+		System.out.println("该句话共" + source.length() + "个字符");
+		System.out.println("该句话去除首尾空格共" + source.trim().length() + "个字符");
+		newline(source);
+		changeencode(source);
 		largeststring(source);
+		sortstring(source);
 	}
 
 	public static void countfor(String str) {
@@ -98,5 +100,31 @@ public class StringNew {
 		System.out.println("分割后最大字符串为：" + temp);
 		
 	}
+    public static void sortstring(String str){
+    	String[] a = str.split("，"); 
+    	String[] result=new String[12];
+    	int pos=0;
+		for(int i=0;i<a.length;i++){  
+			String[] b = a[i].split("、");
+			for(int m=0;m<b.length;m++){
+				String[] c = b[m].split("；");
+				for(int n=0;n<c.length;n++){
+				result[pos++]=c[n];
+				//System.out.println("result"+n + result[n]);
+				}
+			}
+	    }
+		
+		Arrays.sort(result);  
+		for(String str1 : result) {  
+		    //System.out.println("分割后排序结果为："+str1);  
+		}  
+		for(int i=1;i<result.length;i++){
+			if(result[i-1].equals(result[i])){				
+				System.out.println("分割后的字符串有相同的字符串"+i);
+			}			
+		}
+		System.out.println("分割后的字符串没有相同的字符串");
+    }
 
 }
