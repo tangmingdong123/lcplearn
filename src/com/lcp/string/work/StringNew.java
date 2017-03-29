fcd0e02ad38e32f82d252a957f29412e36eb792f
cli <cli@linkernetworks.com>
2017-03-29 10:55:37 +0800

    String new

---
+++ src/com/lcp/string/work/StringNew.java
@@ -0,0 +1,9 @@
+package com.lcp.string.work;
+
+public class StringNew {
+	public static void main(String[] args){
+		char[] helloArray = {'l','i','n','k','e','r'};
+		String helloString = new String(helloArray);
+		System.out.println(helloString);
+	}
+}
