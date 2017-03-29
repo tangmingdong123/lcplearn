dd9d6de6241232b2f7e4be3ba313e5e4fca40c31
cli <cli@linkernetworks.com>
2017-03-29 11:34:52 +0800

    charAt

--- src/com/lcp/string/work/StringNew.java
+++ src/com/lcp/string/work/StringNew.java
@@ -7,12 +7,8 @@ public class StringNew {
 		//System.out.println(helloString);
 		int len = helloString.length();
 		System.out.println(helloString+" "+"length:"+len);
-		Object floatVar = 3.1415926;
-		Object intVar = 4;
-		Object stringVar = "linkernetworks.com";
-		System.out.printf("浮点型变量的的值为 " +
-                "%f, 整型变量的值为 " +
-                " %d, 字符串变量的值为 " +
-                "is %s", floatVar, intVar, stringVar);
+		//charAt()
+		char result = helloString.charAt(3);
+		System.out.println("result:"+result);
 	}
 }
