2fe1c8cbdb9f4e39095a3cd5e164bf4c418a165c
cli <cli@linkernetworks.com>
2017-03-31 11:35:52 +0800

    sort

--- src/com/lcp/bubble/sort/Sort.java
+++ src/com/lcp/bubble/sort/Sort.java
@@ -3,17 +3,21 @@ package com.lcp.bubble.sort;
 public class Sort {
 	public static void sort(int[] num){
 	for(int i=0;i<num.length;i++){
-		int a=num[i];
-		int b=num[i+1];
+		for(int n=1;n<num.length;n++){
+		int a=num[n-1];
+		int b=num[n];
 		if (a>=b){
-			num[i]=b;
-			num[i+1]=a;
+			num[n-1]=b;
+			num[n]=a;
 		}
-		System.out.println(num);
+		}
+	}
+	for(int m=0;m<num.length;m++){
+		System.out.println(num[m]);
 	}
  }
-public static void main(int[] num){
-	int[] num1 = {3,4,5,1,2,7,0,9,8};
-	sort(num1);
+public static void main(String[] args){
+	int[] num = {3,4,5,1,2,7,0,9,8};
+	sort(num);
  }
 }
