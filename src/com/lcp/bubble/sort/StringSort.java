85c4406b2f5c53208c6229edd44cd10640457ce5
mingdongtang <mingdongtang@creditease.cn>
2017-03-31 11:49:48 +0800

    xx

---
+++ src/com/lcp/bubble/sort/StringSort.java
@@ -0,0 +1,14 @@
+package com.lcp.bubble.sort;
+
+public class StringSort {
+	public static void sort(String[] strs){
+	
+ }
+public static void main(String[] args){
+	String[] strs = new String[]{
+			"x","y","z","j","k","l","n","m","o","p","q","s","r","t","u","v","w","g","h","i","e","f","d","c","b","a"
+	};
+	
+	sort(strs);
+ }
+}
