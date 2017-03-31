3f6e662b5782c7383d4f061ca6db306398af6cc1
cli <cli@linkernetworks.com>
2017-03-31 11:55:42 +0800

    string sort

--- src/com/lcp/bubble/sort/StringSort.java
+++ src/com/lcp/bubble/sort/StringSort.java
@@ -2,6 +2,20 @@ package com.lcp.bubble.sort;
 
 public class StringSort {
 	public static void sort(String[] strs){
+		for (int i = 0; i < strs.length; i++) {
+			for (int n = 1; n < strs.length; n++) {
+				String a = strs[n - 1];
+				String b = strs[n];
+				if (a.compareTo(b)>0) {
+					strs[n - 1] = b;
+					strs[n] = a;
+				}
+			}
+		}
+		System.out.print("排序后结果为：");
+		for (int m = 0; m < strs.length; m++) {
+			System.out.print(strs[m] + ",");
+		}
 	
  }
 public static void main(String[] args){
