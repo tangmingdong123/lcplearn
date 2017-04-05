bf7ca44f0cb9aaf508b004e82d57cbbcda99df70
mingdongtang <mingdongtang@creditease.cn>
2017-04-05 18:16:45 +0800

    variant

---
+++ src/com/lcp/variant/S6.java
@@ -0,0 +1,23 @@
+package com.lcp.variant;
+
+public class S6 {
+	private static String size = "16cm*32cm";//手机的大小，S6系列手机都是这一个大小，所以是static的
+	
+	private String buyDate = null;//购买日期，每一个手机都有自己的购买日期，所以不是static的
+	
+	public static void printS6(){
+		System.out.println("S6.size="+S6.size);
+	}
+	
+	public void printBuyDate(){
+		System.out.println("S6.size="+buyDate);
+	}
+	
+	public static void main(String[] args) {
+		S6.printS6();
+		
+		S6 tmdS6 = new S6();
+		tmdS6.buyDate = "2015--05-28";
+		tmdS6.printBuyDate();
+	}
+}
