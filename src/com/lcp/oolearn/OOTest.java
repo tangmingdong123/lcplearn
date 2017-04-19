76b74f9aa89930f59c252a6c23e42b57594c1b89
cli <cli@linkernetworks.com>
2017-04-19 18:43:03 +0800

    5

--- src/com/lcp/oolearn/OOTest.java
+++ src/com/lcp/oolearn/OOTest.java
@@ -3,11 +3,22 @@ package com.lcp.oolearn;
 public class OOTest {
 	public static void main(String[] args){
 		int[] wheel = {4,6,8,10,12};  
+		int[] capacity={10,20,30,40,50};
 		Truck[] trucks = new Truck[5];  
+		Bus[] bus = new Bus[5];
+		int wheelNum=0;
+		int capacityNum=0;
 		for(int i=0;i<5;i++){
 			trucks[i] = new Truck();
 			trucks[i].setWheels(wheel[i]);
-			System.out.println(trucks[i].getWheels());
+			wheelNum=wheelNum+trucks[i].getWheels();		
 	}	
+		for(int i=0;i<5;i++){
+			bus[i] = new Bus();
+			bus[i].setCapacity(capacity[i]);
+			capacityNum=capacityNum+bus[i].getCapacity();		
+	}
+		System.out.println(wheelNum);
+		System.out.println(capacityNum);
   }
 }
